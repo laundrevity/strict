47f2f0d692bd01e3e003759ac187d5cbd918d453
Conor Mahany <mahany25@gmail.com>
2022-02-10 02:00:18 +0100

    added final attributes/methods

--- ext_abc.py
+++ ext_abc.py
@@ -6,6 +6,10 @@ logging.basicConfig(
 )
 
 
+class FinalAttributeException(Exception):
+    pass
+
+
 class DummyAttribute:
     pass
 
@@ -15,17 +19,31 @@ class DummyMethod:
         return None
 
 
-def abstractattribute(obj=None):
+def abstract_attribute(obj=None):
     if obj is None:
         obj = DummyAttribute()
     obj.__is_abstract_attribute__ = True
     return obj
 
 
-def abstractmethod(obj=None):
+def abstract_method(obj=None):
     if obj is None:
         obj = DummyMethod()
     obj.__is_abstract_method__ = True
+    return obj
+
+
+def final_attribute(obj=None):
+    if obj is None:
+        obj = DummyAttribute()
+    obj.__is_final_attribute__ = True
+    return obj
+
+
+def final_method(obj=None):
+    if obj is None:
+        obj = DummyMethod()
+    obj.__is_final_method__ = True
     return obj
 
 
@@ -42,11 +60,11 @@ class StrictMeta(type):
 
     def __call__(cls, *args, **kwargs):
         instance = super().__call__(*args, **kwargs)
+        keys = [key for key in dir(instance) if not key.startswith('__')]
 
         abstract_attributes = {
-            key for key in dir(instance)
-            if not key.startswith('__')
-            and getattr(
+            key for key in keys
+            if getattr(
                 getattr(instance, key),
                 '__is_abstract_attribute__',
                 False
@@ -61,10 +79,12 @@ class StrictMeta(type):
             )
 
         abstract_methods = {
-            key for key in dir(instance)
-            # if isinstance(getattr(instance, key), Callable)
-            if not key.startswith('__')
-            and getattr(getattr(instance, key), '__is_abstract_method__', False)
+            key for key in keys
+            if getattr(
+                getattr(instance, key),
+                '__is_abstract_method__',
+                False
+            )
         }
         if abstract_methods:
             raise NotImplementedError(
@@ -74,19 +94,50 @@ class StrictMeta(type):
                 )
             )
 
-        logging.info(f"{cls.__bases__=}")
+        # logging.info(f"{cls.__bases__=}")
         parent = cls.__bases__[0]
         invalid_methods = {
-            key for key in dir(instance)
+            key for key in keys
             if not isinstance(getattr(instance, key), Callable)
-            and not key.startswith('__')
             and getattr(getattr(parent, key), '__is_abstract_method__', False)
         }
         if invalid_methods:
             raise NotImplementedError(
                 (
                     f"Cannot instantiate abstract class {cls.__name__} "
-                    f"with non-callable methods {' ,'.join(invalid_methods)}"
+                    f"with non-callable methods {', '.join(invalid_methods)}"
+                )
+            )
+
+        # logging.info(f"{cls.__name__=}")
+        # for key in keys:
+        #     logging.info((
+        #         f"getattr(instance, {key}) = {getattr(instance, key)}"
+        #     ))
+
+        final_attributes = {
+            key for key in keys
+            if getattr(getattr(parent, key), '__is_final_attribute__', False)
+            and not getattr(getattr(instance, key), '__is_final_attribute__', False)
+        }
+        if final_attributes:
+            raise NotImplementedError(
+                (
+                    f"Cannot instantiate abstract class {cls.__name__} "
+                    f"with final attributes {', '.join(final_attributes)}"
+                )
+            )
+
+        final_methods = {
+            key for key in keys
+            if getattr(getattr(parent, key), '__is_final_method__', False)
+            and not getattr(getattr(instance, key), '__is_final_method__', False)
+        }
+        if final_methods:
+            raise NotImplementedError(
+                (
+                    f"Cannot instantiate abstract class {cls.__name__} "
+                    f"with final methods {', '.join(final_methods)}"
                 )
             )
 
--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
-from ext_abc import StrictMeta, abstractattribute, abstractmethod
+# Conor L. Mahany, 2022-02-09
+from ext_abc import StrictMeta, abstract_attribute, abstract_method, final_attribute, final_method
 from typing import Any
 import logging
 import pytest
@@ -8,15 +9,23 @@ logging.basicConfig(
 
 
 class StrictClassInterface(metaclass=StrictMeta):
-    @abstractattribute
+    @abstract_attribute
     def necessary_attribute(self) -> Any: ...
 
-    @abstractmethod
+    @abstract_method
     def necessary_method(self): ...
 
     def optional_attribute(self) -> Any: ...
 
     def optional_method(self): ...
+
+    @final_attribute
+    def illegal_attribute(self) -> Any:
+        return "you mustn't change this..."
+
+    @final_method
+    def illegal_method(self, x: float):
+        return x*x+1
 
 
 class MissingAttribute(StrictClassInterface):
@@ -53,6 +62,24 @@ class MaximalStrictClass(StrictClassInterface):
         pass
 
 
+class BadClassA(StrictClassInterface):
+    necessary_attribute = None
+    illegal_attribute = 'orly??'
+
+    def necessary_method(self):
+        return None
+
+
+class BadClassB(StrictClassInterface):
+    necessary_attribute = None
+
+    def necessary_method(self):
+        return None
+
+    def illegal_method(self):
+        return 'whatcha gonna do about it??'
+
+
 def test_main():
     try:
         _ = MissingAttribute()
@@ -74,3 +101,16 @@ def test_main():
 
     assert MinimalStrictClass()
     assert MaximalStrictClass()
+
+
+def test_final():
+    try:
+        bc = BadClassA()
+        a = 1/0
+    except NotImplementedError as e:
+        logging.info(e)
+
+    try:
+        BadClassB()
+    except NotImplementedError as e:
+        logging.info(e)
